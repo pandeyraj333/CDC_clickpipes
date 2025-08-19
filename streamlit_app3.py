bc661f9e7ace2ebac2dd6b671bcc0aedb5c122b3
pandeyraj333 <139428980+pandeyraj333@users.noreply.github.com>
2025-08-19 16:05:54 +0530

    Update streamlit_app3.py

--- streamlit_app3.py
+++ streamlit_app3.py
@@ -70,11 +70,11 @@ st.subheader("🌡️ Latest Live Weather Snapshot")
 query_city = f"""
 SELECT
     city,
-FROM (
-SELECT city, parseDateTimeBestEffort(timestamp) AS parsed_timestamp, argMax(_peerdb_is_deleted, _peerdb_synced_at) AS latest_deleted from live_weather_db_weather_data group by city, parsed_timestamp
-)
-where latest_deleted = 0
-GROUP BY city
+    FROM (
+        SELECT city, parseDateTimeBestEffort(timestamp) AS parsed_timestamp, argMax(_peerdb_is_deleted, _peerdb_synced_at) AS latest_deleted from live_weather_db_weather_data group by city, parsed_timestamp
+        )
+    where latest_deleted = 0
+    GROUP BY city;
 """
 
 result = client.query(query_city)
